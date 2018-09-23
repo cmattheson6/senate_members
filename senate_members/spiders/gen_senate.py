ec459f5a909d39ab133669a681d6f1df54032ea3
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-09-23 12:07:10 -0400

    Update gen_senate.py

--- senate_members/spiders/gen_senate.py
+++ senate_members/spiders/gen_senate.py
@@ -5,12 +5,11 @@ import scrapy
 class GenSenateSpider(scrapy.Spider):
     name = 'gen_senate'
     allowed_domains = ['http://www.senate.gov/general/contact_information/senators_cfm.xml']
-    start_urls = ['http://www.senate.gov/general/contact_information/senators_cfm.xml']
+    start_url = 'http://www.senate.gov/general/contact_information/senators_cfm.xml'
     
     # sends a request to the senate page to pull date
     def start_requests(self):
-        for url in start_urls:
-            yield scrapy.Request(url = url, callback = self.parse)
+            yield scrapy.Request(url = start_url, callback = self.parse)
             
     # pulls the needed data out of the senate page
     def parse(self, response):
