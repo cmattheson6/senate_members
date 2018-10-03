450f693cd0d075b8ae947fe1c7cceac7817ca333
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-10-03 13:02:15 -0400

    Update gen_senate.py

--- senate_members/spiders/gen_senate.py
+++ senate_members/spiders/gen_senate.py
@@ -14,7 +14,9 @@ class GenSenateSpider(scrapy.Spider):
     # pulls the needed data out of the senate page
     def parse(self, response):
         if response.status == 403:
-            yield scrapy.Request(url = start_url, callback = self.parse, headers = headers)
+            start_url = 'http://www.senate.gov/general/contact_information/senators_cfm.xml'
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
+            yield scrapy.Request(url = start_url, callback = self.parse, headers = headers);
         else:
             print(response.status)
             pass;
